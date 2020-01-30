f43d77e2362c57677ed196d0dd2258cd6bbf0924
Elton Barbosa Gomes <eltonbarbosagomes@gmail.com>
2020-01-30 07:25:36 -0300

    atualizacao de classes

--- core/admin.py
+++ core/admin.py
@@ -4,15 +4,26 @@ from .models import (
     Marca, 
     Veiculo, 
     Parametros, 
-    MovRotativo
+    MovRotativo,
+    Mensalista,
+    MovMensalista
 )
 
 class MovRotativoAdmin(admin.ModelAdmin):
     list_display = (
-        'checkin', 'checkout', 'valor_hora', 'pago', 'total', 'horas_total')
+        'checkin', 'checkout', 'valor_hora', 'pago', 'total',
+        'horas_total', 'veiculo')
+
+
+class MovMensalistaAdmin(admin.ModelAdmin):
+    list_display = (
+        'mensalista', 'dt_pgto', 'total')
+
 
 admin.site.register(Pessoa)
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Parametros)
+admin.site.register(Mensalista)
+admin.site.register(MovMensalista, MovMensalistaAdmin)
 admin.site.register(MovRotativo, MovRotativoAdmin)
--- core/models.py
+++ core/models.py
@@ -56,3 +56,21 @@ class MovRotativo(models.Model):
 
     def __str__(self):
         return self.veiculo.nome + ' - ' + self.veiculo.placa
+
+
+class Mensalista(models.Model):
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+    inicio = models.DateField()
+    valor_mes = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.veiculo) + ' - ' + str(self.inicio)
+
+
+class MovMensalista(models.Model):
+    mensalista = models.ForeignKey(Mensalista, on_delete=models.CASCADE)
+    dt_pgto = models.DateField()
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.mensalista) + ' - ' + str(self.total)
